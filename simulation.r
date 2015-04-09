e01fec536deeb87540e7307afd66c150078d922a
Tyler Beason <tbeas12@gmail.com>
2015-04-08 23:23:49 -0500

    finished project

--- simulation.r
+++ simulation.r
@@ -11,6 +11,9 @@ CATprice[,1] <- as.Date(CATprice[,1], format="%m-%d-%Y")
 CAT.xts <- as.xts(CATprice[,-1], order.by=CATprice[,1])
 names(CAT.xts) <- "CAT"
 
+tbill[,1] <- as.Date(tbill[,1], format="%m-%d-%Y")
+tbill.xts <- as.xts(tbill[,-1], order.by=tbill[,1])
+
 #calc monthly returns
 for(i in 1:ncol(ind.xts)){
   if(i == 1){
@@ -55,17 +58,16 @@ for(i in 2:length(expected))
   priorC <- pnorm(retS,mean=mean(retS),sd=sd(retS))
   priorD <- c(priorC[1],diff(priorC))
   
-  likelihood <- rnorm(length(priorD),mean=mean(rC[1:i]),sd=sd(rC[1:i]))
+  likelihood <- pnorm(rnorm(length(priorD),mean=mean(rC[1:i]),sd=sd(rC[1:i])),mean=mean(rC[1:i]),sd=sd(rC[1:i]))
   joint <- priorD*likelihood
   
   posterior <- joint/sum(joint)
   weighted <- posterior*retS
-  expected[i] <- sum(weighted)  
+  expected[i] <- sum(weighted) 
   stdev[i] <- sqrt(sum((priorD-expected[i])^2*posterior))
 }
-matplot(cbind(rC[1:41],expected),type="l",main="Strictly Distribution Based")
-samples1 <- rnorm(1000,mean=expected[length(expected)],sd=stdev[length(expected)])
-guess1 <- mean(samples1)
+matplot(cbind(rC[2:42],expected),type="l",main="Strictly Distribution Based")
+samples1 <- data.frame(ret=rnorm(1000,mean=expected[length(expected)],sd=stdev[length(expected)]))
 
 
 #### projection 2
@@ -93,7 +95,7 @@ CAT.beta <- apply.monthly(CAT.daily,FUN=CAPM.beta,benchmark)
 expCAPM <- numeric(length(rC)-1)
 sdCAPM <- numeric(length(rC)-1)
 
-retI <- as.numeric(betas[1,])*as.numeric(benchMonthly[1])
+retI <- as.numeric(tbill.xts[1])+as.numeric(betas[1,])*(as.numeric(benchMonthly[1])-as.numeric(tbill.xts[1]))
 retS <- sort(retI)
 priorC <- pnorm(retS,mean=mean(retS),sd=sd(retS))
 priorD <- c(priorC[1],diff(priorC))
@@ -106,12 +108,12 @@ sdCAPM[1] <- sqrt(sum((priorD-expCAPM[1])^2*posterior1))
 
 for(i in 2:length(expCAPM))
 {
-  retI <- as.numeric(betas[i,])*as.numeric(benchMonthly[i])  
+  retI <- as.numeric(tbill.xts[i])+as.numeric(betas[i,])*(as.numeric(benchMonthly[i])-as.numeric(tbill.xts[i]))  
   retS <- sort(retI)
   priorC <- pnorm(retS,mean=mean(retS),sd=sd(retS))
   priorD <- c(priorC[1],diff(priorC))
   
-  likelihood <- rnorm(length(priorD),mean=mean(rC[1:i]),sd=sd(rC[1:i]))
+  likelihood <- pnorm(rnorm(length(priorD),mean=mean(rC[1:i]),sd=sd(rC[1:i])),mean=mean(rC[1:i]),sd=sd(rC[1:i]))
   joint <- priorD*likelihood
   
   posterior <- joint/sum(joint)
@@ -119,6 +121,23 @@ for(i in 2:length(expCAPM))
   expCAPM[i] <- sum(weighted)  
   sdCAPM[i] <- sqrt(sum((priorD-expCAPM[i])^2*posterior))
 }
-matplot(cbind(rC[1:41],expCAPM),type="l",main="Using CAPM")
-samples2 <- rnorm(1000,mean=expCAPM[length(expCAPM)],sd=sdCAPM[length(expCAPM)])
-guess2 <- mean(samples2)
+matplot(cbind(rC[2:42],expCAPM),type="l",main="Using CAPM")
+samples2 <- data.frame(ret=rnorm(1000,mean=expCAPM[length(expCAPM)],sd=sdCAPM[length(expCAPM)]))
+
+mseBayes <- sqrt((expected-rC[2:42])^2)
+mseCAPM <- sqrt((expCAPM-rC[2:42])^2)
+summary(mseBayes)
+summary(mseCAPM)
+
+ret <- merge(CAT.ret[2:42],expected,expCAPM)
+names(ret) <- c("CAT","Plain","CAPM")
+chart.TimeSeries(ret,legend.loc="topright",main="CAT Returns - Estimated vs Actual",ylab="Monthly Return",cex.legend=0.6)
+
+
+samples1$type <- 'plain'
+samples2$type <- 'CAPM'
+samp <- rbind(samples1,samples2)
+ggplot(samp, aes(ret, fill = type)) + 
+  geom_histogram(alpha = 0.5, position = 'identity') +
+  xlab("Return") +
+  ggtitle("Histogram of Final Period Returns")
